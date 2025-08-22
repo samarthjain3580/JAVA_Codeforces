8b57845b55985f0cd7ab3e1a63c790acfb6ec6a8
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-08-22 23:19:44 +0530

    Create Catch_the_Coin.java

---
+++ CodeForces/Catch_the_Coin.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Catch_the_Coin {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+        	int a = sc.nextInt();
+        	int b = sc.nextInt();
+        	if (b>-2) {System.out.println("YES");}
+        	else {System.out.println("NO");}
+        }
+  }
+}
