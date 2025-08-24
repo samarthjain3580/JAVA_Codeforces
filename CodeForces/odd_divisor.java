c63d9d622e8b096fefcc3332204457eb2eb0d4c8
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-08-24 12:06:28 +0530

    Update odd_divisor.java

--- CodeForces/odd_divisor.java
+++ CodeForces/odd_divisor.java
@@ -1,49 +1,15 @@
-package CodeForces;
-// import java.util.Scanner;
-// public class word_game {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         int t = sc.nextInt(); 
-//         while(t-->0) {
-//             int n = sc.nextInt();
-//             for (int i=0; i<n; i++) {
-//                 String a = sc.next()+" ";
-//                 String b = sc.next()+" ";
-//                 String c = sc.next()+" ";
-//             }
-//         }
-//         sc.close();
-//     }
-// }
 import java.util.*;
-public class odd_divisor {
+public class oddDivisor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt(); 
         while (t-->0) {
-            // boolean flag = true;
             long n = sc.nextLong();
-            // for (long i=2; i<=n; i++) {
-            //     if (n%i == 0){
-            //         if (i%2 != 0) {
-            //             System.out.println("YES");
-            //             flag = false;
-            //             break;
-            //         }
-            //     }
-            // }
             while(n%2==0){
                 n/=2;
             }
-            if (n!=1)
-                System.out.println("YES");
-            else
-                System.out.println("NO");
-            
-            // if (flag == true) {
-            //     System.out.println("NO");
-            // }
+            if (n!=1) System.out.println("YES");
+            else System.out.println("NO");
         }
-        sc.close();
     }
 }
