6d06eaefeebd4a04ead9453a4c66829264af877c
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-08-24 12:46:57 +0530

    Update yogurt_sale.java

--- CodeForces/yogurt_sale.java
+++ CodeForces/yogurt_sale.java
@@ -1,25 +1,18 @@
-package CodeForces;
 import java.util.*;
 public class yogurt_sale {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt(); 
-        while(t > 0) {
-            int min_cost = 0;
+        while (t-->0) {
             int n = sc.nextInt();
             int a = sc.nextInt();
             int b = sc.nextInt();
-            if (2*a < b ) {
-                min_cost+= n*a;
-            }
-            else{
-                min_cost+= (n/2)*b+a*(n%2);
-            }
-            System.out.println(min_cost);
-
-            t--;
             
-        } 
-        sc.close();
-    }  
+            int first = a*n;
+            int second = (n/2)*b + (n%2)*a;
+            
+            System.out.println(Math.min(first, second));
+        }
+    }
 }
+
