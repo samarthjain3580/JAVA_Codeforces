656416240766dd7f2b32db3b3ce8b439a466ed0a
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-09-13 22:10:25 +0530

    Create sublime_sequences.java

---
+++ CodeForces/sublime_sequences.java
@@ -0,0 +1,20 @@
+package Codeforces;
+import java.util.*;
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0) {
+            int x = sc.nextInt();
+            int n = sc.nextInt();
+            
+            if(n%2 == 0) {
+                System.out.println(0);
+            }
+            else {
+                System.out.println(x);
+            }
+        }
+        sc.close();
+    }
+}
