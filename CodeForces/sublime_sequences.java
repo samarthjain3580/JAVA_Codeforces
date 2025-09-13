7ca97ddb853960716c90e0cb3271eaf27683e454
samarthjain3580 <153626196+samarthjain3580@users.noreply.github.com>
2025-09-13 22:11:11 +0530

    Update sublime_sequences.java

--- CodeForces/sublime_sequences.java
+++ CodeForces/sublime_sequences.java
@@ -1,6 +1,6 @@
 package Codeforces;
 import java.util.*;
-public class main {
+public class sublime_sequences {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
