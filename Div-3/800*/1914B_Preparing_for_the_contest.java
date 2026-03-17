364760b2a5f2d8bdc9e463214958e7ffe4e1b042
Samarth jain <153626196+samarthjain3580@users.noreply.github.com>
2026-03-17 15:37:33 +0530

    Create 1914B_Preparing_for_the_contest.java

---
+++ Div-3/800*/1914B_Preparing_for_the_contest.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            
+            for(int i=n-k; i<=n; i++) {
+                System.out.print(i+" ");
+            }
+            for(int i=n-k-1; i>=1; i--) {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
